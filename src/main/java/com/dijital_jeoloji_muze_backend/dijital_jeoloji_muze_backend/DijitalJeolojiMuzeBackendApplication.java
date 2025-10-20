ca856b2ab6f2a9dc5d7374ad1f2bae189efde186
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-10-20 16:27:47 +0300

    DJM-8: Implement login API endpoint

---
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/DijitalJeolojiMuzeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DijitalJeolojiMuzeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DijitalJeolojiMuzeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/DijitalJeolojiMuzeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DijitalJeolojiMuzeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
