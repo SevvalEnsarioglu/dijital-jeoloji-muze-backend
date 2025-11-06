e08e3cf5b81417f269e184ee577db0708cf356bd
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-06 21:58:46 +0300

    ZiyaretSaatleri entity'si olusturuldu

---
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/entity/ZiyaretSaatleri.java
@@ -0,0 +1,27 @@
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = "ziyaret_saatleri")
+public class ZiyaretSaatleri extends BaseEntity {
+
+    @Field(name = "gun")
+    private String gun;
+
+    @Field(name = "acilis_saati")
+    private LocalTime acilisSaati;
+
+    @Field(name = "kapanis_saati")
+    private LocalTime kapanisSaati;
+}
