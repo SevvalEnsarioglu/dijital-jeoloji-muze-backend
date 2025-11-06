dd7052692cb692bfc055365b153681e4e2eec870
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-06 21:58:37 +0300

    Iletisim entity'si olusturuldu

--- src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/entity/Iletisim.java
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/entity/Iletisim.java
@@ -1,7 +1,17 @@
 package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = "iletisim")
 public class Iletisim extends BaseEntity {
 
     @Field(name = "ad")
