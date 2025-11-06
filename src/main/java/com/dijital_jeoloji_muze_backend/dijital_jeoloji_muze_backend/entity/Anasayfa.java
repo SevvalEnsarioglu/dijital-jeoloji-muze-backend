a1613e65c6f8baca72e02da6d5c6371dbd47d9b9
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-06 21:52:17 +0300

    Anasayfa entity'si olusturuldu

---
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/entity/Anasayfa.java
@@ -0,0 +1,26 @@
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = "anasayfa")
+public class Anasayfa extends BaseEntity {
+    @Field(name = "isim")
+    private String isim;
+
+    @Field(name = "foto_id")
+    private ObjectId fotoId;
+
+    @Field(name= "aciklama")
+    private String aciklama;
+
+}
