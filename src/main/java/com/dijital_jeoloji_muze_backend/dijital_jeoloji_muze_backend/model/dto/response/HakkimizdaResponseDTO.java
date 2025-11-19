bc71eb3957825a0ffd352a0b08f84fd2a588b2e5
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-19 16:19:38 +0300

    Hakkimizda Entity'sinin DTO'ları oluşturuldu

---
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/model/dto/response/HakkimizdaResponseDTO.java
@@ -0,0 +1,17 @@
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.Instant;
+
+public record HakkimizdaResponseDTO(
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        Long id,
+        String hakkinda,
+        String adres,
+        String telefon,
+        String email,
+        Instant createdAt,
+        Instant updatedAt
+) {
+}
