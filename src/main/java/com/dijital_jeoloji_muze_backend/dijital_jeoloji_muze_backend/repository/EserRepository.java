2315f2e84f7b87d4a4de51aaf36f5c3e36a8af38
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-19 15:24:53 +0300

    EserRepository olusturuldu

---
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/repository/EserRepository.java
@@ -0,0 +1,11 @@
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.repository;
+
+import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.entity.Eser;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EserRepository extends MongoRepository<Eser, ObjectId> {
+
+}
