624ec3df1a91a0fbe93e1bbac446691f0ab5f43a
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-12-03 16:02:26 +0300

    Config dosyası açıldı ve Cors Config eklendi

--- src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/config/CorsConfig.java
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend;
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
