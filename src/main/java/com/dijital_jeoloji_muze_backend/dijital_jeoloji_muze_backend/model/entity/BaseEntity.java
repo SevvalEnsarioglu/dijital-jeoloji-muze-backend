ffc18b323b3de147e3b6725271738cd702b1d0ea
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-12-12 19:56:12 +0300

    İyileştirme güncellemesi yapıldı

--- src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/model/entity/BaseEntity.java
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/model/entity/BaseEntity.java
@@ -5,7 +5,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.Persistable;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
 
