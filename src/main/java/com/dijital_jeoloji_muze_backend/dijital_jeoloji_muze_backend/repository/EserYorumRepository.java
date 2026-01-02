425dc93bca5218516c5a21fa51c8c845af60d62e
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-01-02 17:02:21 +0300

    EserYorum Repository güncellendi

--- src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/repository/EserYorumRepository.java
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/repository/EserYorumRepository.java
@@ -1,9 +1,14 @@
 package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.repository;
 
 import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.entity.EserYorum;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EserYorumRepository extends MongoRepository<EserYorum, Integer> {
+    List<EserYorum> findByEserID(Integer eserID, Sort sort);
+
 }
