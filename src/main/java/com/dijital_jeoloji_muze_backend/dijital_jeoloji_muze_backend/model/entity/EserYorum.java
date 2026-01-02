334a7093afcb760c9a9eaa9ad3d8ddc0843ccce1
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-01-02 16:20:08 +0300

    EserYorum entity güncellendi-3

--- src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/model/entity/EserYorum.java
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/model/entity/EserYorum.java
@@ -1,5 +1,7 @@
 package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.entity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -30,6 +32,12 @@ public class EserYorum extends BaseEntity<Integer> {
 
     @Field(name="puan")
     @Indexed
+    @Min(value = 1, message = "Puan en az 1 olabilir")
+    @Max(value = 5, message = "Puan en fazla 5 olabilir")
     private Integer puan;
+
+    //buraya okundu ekleme sebebimiz admin hangi yorumların görüneceğini belirleyebilsin diye
+    @Field(name = "okundu")
+    private Boolean okundu = true;
 
 }
