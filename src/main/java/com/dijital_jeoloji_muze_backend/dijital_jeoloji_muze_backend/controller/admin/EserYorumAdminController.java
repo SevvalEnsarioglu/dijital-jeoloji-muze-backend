f2ea4eea04c9bf6b3326e7f2f2064f7c794423b0
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-01-05 01:59:14 +0300

    EserYorumController'ları oluşturuldu

---
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/controller/admin/EserYorumAdminController.java
@@ -0,0 +1,52 @@
+package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.controller.admin;
+
+import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.dto.request.EserYorumRequestDTO;
+import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.dto.request.OkunduDurumuRequestDTO;
+import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.dto.response.EserYorumResponseDTO;
+import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.service.EserYorumService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/eser-yorum")
+@RequiredArgsConstructor
+public class EserYorumAdminController {
+    private final EserYorumService eserYorumService;
+
+    //var olan tüm yorumlar
+    @GetMapping
+    public ResponseEntity<List<EserYorumResponseDTO>> getAllEserYorum() {
+        List<EserYorumResponseDTO> yorumList = eserYorumService.getAllEserYorum();
+        return ResponseEntity.ok(yorumList);
+    }
+
+    //ilgili yorum id si ile tek bir yorum
+    @GetMapping("/{id}")
+    public ResponseEntity<EserYorumResponseDTO> getEserYorumById(@PathVariable Integer id) {
+        EserYorumResponseDTO yorum = eserYorumService.getEserYorumById(id);
+        return ResponseEntity.ok(yorum);
+    }
+    // eser yorum güncelleme (admin isterse yani ama sebebini gereksinimini düşünmedim)
+    @PutMapping("/{id}")
+    public ResponseEntity<EserYorumResponseDTO> updateEserYorum(@PathVariable Integer id, @Valid @RequestBody EserYorumRequestDTO request) {
+        EserYorumResponseDTO updated = eserYorumService.updateEserYorum(id, request);
+        return ResponseEntity.ok(updated);
+    }
+
+    //yorum okundu durumu güncelleme
+    @PatchMapping("/{id}/okundu")
+    public ResponseEntity<EserYorumResponseDTO> updateOkunduDurumu(@PathVariable Integer id, @RequestBody OkunduDurumuRequestDTO request) {
+        EserYorumResponseDTO updated = eserYorumService.updateOkunduDurumu(id, request.okundu());
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEserYorum(@PathVariable Integer id) {
+        eserYorumService.deleteEserYorum(id);
+        return ResponseEntity.noContent().build();
+    }
+}
