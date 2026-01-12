fcfec5340f124cbe54c86ff27fd2b11a9bc238a4
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-01-12 13:54:17 +0300

    EserRepository Sort için findByIsimContainingIgnoreCase eklendi

--- src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/repository/EserRepository.java
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/repository/EserRepository.java
@@ -1,10 +1,17 @@
 package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.repository;
 
 import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.entity.Eser;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EserRepository extends MongoRepository<Eser, Integer> {
+
+    @Query(value = "{ 'isim': { $regex: ?0, $options: 'i' } }")
+    List<Eser> findByIsimContainingIgnoreCase(String isim, Sort sort);
 
 }
