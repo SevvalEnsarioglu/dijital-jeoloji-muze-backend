49206ee0a2d692e4a5ea26e6e5cc0cddfc05fb6b
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2026-01-12 16:31:06 +0300

    EserService dto güncellemesi

--- src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/service/EserService.java
+++ src/main/java/com/dijital_jeoloji_muze_backend/dijital_jeoloji_muze_backend/service/EserService.java
@@ -1,6 +1,7 @@
 package com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.service;
 
 import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.dto.request.EserRequestDTO;
+import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.dto.response.EserListResponseDTO;
 import com.dijital_jeoloji_muze_backend.dijital_jeoloji_muze_backend.model.dto.response.EserResponseDTO;
 
 import java.util.List;
@@ -9,12 +10,11 @@ public interface EserService {
 
     EserResponseDTO createEser(EserRequestDTO request);
 
-
     EserResponseDTO getEserById(Integer id);
 
     EserResponseDTO updateEser(Integer id, EserRequestDTO request);
 
     void deleteEser(Integer id);
 
-    List<EserResponseDTO> getAllEser(String isim, String sortBy, String sortDirection);
+    List<EserListResponseDTO> getAllEser(String isim, String sortBy, String sortDirection);
 }
